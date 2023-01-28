d4b160dff6bd201f817d86a71ea84fe9e3e2d275
Алексей Ануфриенок <alex.anufrienok@gmail.com>
2023-01-28 13:41:00 +0300

    fix#1- added open class on hover on cart

---
+++ test-work/admin/config.php
@@ -0,0 +1,34 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://test-work/admin/');
+define('HTTP_CATALOG', 'http://test-work/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://test-work/admin/');
+define('HTTPS_CATALOG', 'http://test-work/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OpenServer/domains/test-work/admin/');
+define('DIR_SYSTEM', 'C:/OpenServer/domains/test-work/system/');
+define('DIR_IMAGE', 'C:/OpenServer/domains/test-work/image/');
+define('DIR_LANGUAGE', 'C:/OpenServer/domains/test-work/admin/language/');
+define('DIR_TEMPLATE', 'C:/OpenServer/domains/test-work/admin/view/template/');
+define('DIR_CONFIG', 'C:/OpenServer/domains/test-work/system/config/');
+define('DIR_CACHE', 'C:/OpenServer/domains/test-work/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/OpenServer/domains/test-work/system/storage/download/');
+define('DIR_LOGS', 'C:/OpenServer/domains/test-work/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/OpenServer/domains/test-work/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/OpenServer/domains/test-work/system/storage/upload/');
+define('DIR_CATALOG', 'C:/OpenServer/domains/test-work/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'test-work');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCartForum API
+define('OPENCARTFORUM_SERVER', 'https://opencartforum.com/');
---
+++ test-work/config.php
@@ -0,0 +1,33 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://test-work/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://test-work/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OpenServer/domains/test-work/catalog/');
+define('DIR_SYSTEM', 'C:/OpenServer/domains/test-work/system/');
+define('DIR_IMAGE', 'C:/OpenServer/domains/test-work/image/');
+define('DIR_LANGUAGE', 'C:/OpenServer/domains/test-work/catalog/language/');
+define('DIR_TEMPLATE', 'C:/OpenServer/domains/test-work/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/OpenServer/domains/test-work/system/config/');
+define('DIR_CACHE', 'C:/OpenServer/domains/test-work/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/OpenServer/domains/test-work/system/storage/download/');
+define('DIR_LOGS', 'C:/OpenServer/domains/test-work/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/OpenServer/domains/test-work/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/OpenServer/domains/test-work/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'test-work');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// Redis
+// define('CACHE_HOSTNAME', 'localhost');
+// define('CACHE_PORT', '6379');
+// define('CACHE_PREFIX', 'oc_');
